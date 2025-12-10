6e80c50dc48b3e4ce2d6e5cd94a4fd77a6219d1f
Mitko <wido9696@gmail.com>
2025-12-10 23:10:32 +0200

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ src/main/java/com/example/finalprogressreview/FinalProgressReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprogressreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProgressReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProgressReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalprogressreview/FinalProgressReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprogressreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProgressReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
